08e1355bd0e3d4bdf2eb06f8360de7cc46f7c5b6
alex <xor@alexs-MacBook-Pro.local>
2022-09-28 01:11:20 +0300

    next

--- stable_diffusion_videos/stable_diffusion_pipeline.py
+++ stable_diffusion_videos/stable_diffusion_pipeline.py
@@ -20,31 +20,6 @@ from scipy import integrate
 from diffusers.configuration_utils import ConfigMixin, register_to_config
 from diffusers.schedulers.scheduling_utils import SchedulerMixin, SchedulerOutput
 
-# Copyright 2022 Katherine Crowson, The HuggingFace Team and hlky. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from typing import Optional, Tuple, Union
-
-import numpy as np
-import torch
-
-from scipy import integrate
-
-from .configuration_utils import ConfigMixin, register_to_config
-from .scheduling_utils import SchedulerMixin, SchedulerOutput
-
-
 class EulerDiscreteScheduler(SchedulerMixin, ConfigMixin):
     """
     Implements Algorithm 2 (Euler steps) from Karras et al. (2022).
