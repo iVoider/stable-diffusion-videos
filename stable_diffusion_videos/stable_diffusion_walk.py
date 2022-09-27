0d0c33718ab3606946f203f6abb4a6047f5340f7
alex <xor@alexs-MacBook-Pro.local>
2022-09-27 22:14:26 +0300

    removed format

--- stable_diffusion_videos/stable_diffusion_walk.py
+++ stable_diffusion_videos/stable_diffusion_walk.py
@@ -147,8 +147,6 @@ class EulerDiscreteScheduler(SchedulerMixin, ConfigMixin):
         noise: Union[torch.FloatTensor, np.ndarray],
         timesteps: Union[torch.IntTensor, np.ndarray],
     ) -> Union[torch.FloatTensor, np.ndarray]:
-        if self.tensor_format == "pt":
-            timesteps = timesteps.to(self.sigmas.device)
         sigmas = self.match_shape(self.sigmas[timesteps], noise)
         noisy_samples = original_samples + noise * sigmas
 
